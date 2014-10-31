b0e63a7a79e5c1bb6d04e5c4fa667ae472acabc3
Patrick Kimber <code@pkimber.net>
2014-10-31 13:19:43 +0000

    backup script - files and postgres #618

--- web/backup.sh
+++ web/backup.sh
@@ -6,28 +6,35 @@ set -u
 # backup {{ site }}
 {% set rsync = gpg['rsync'] -%}
 
+# dump database
 DUMP_FILE=/home/web/repo/backup/{{ site }}/$(date +"%Y%m%d_%H%M").sql
 pg_dump -U postgres {{ site_name }} -f $DUMP_FILE
-echo "{{ site_name }}.backup.dump:1|c" | nc -w 1 -u {{ django['monitor'] }} 2003
+# Send a metric to statsd from bash
+# nstielau / send_metric_to_statsd.sh
+# https://gist.github.com/nstielau/966835
+#
+# Useful for:
+#   deploy scripts (http://codeascraft.etsy.com/2010/12/08/track-every-release/)
+#   init scripts
+#   sending metrics via crontab one-liners
+#   sprinkling in existing bash scripts.
+#
+# netcat options:
+#   -w timeout If a connection and stdin are idle for more than timeout seconds, then the connection is silently closed.
+#   -u         Use UDP instead of the default option of TCP.
+echo "{{ site_name }}.backup.db.dump:1|c" | nc -w 1 -u {{ django['monitor'] }} 2003
 
+# backup database
+duplicity full --encrypt-key="{{ rsync['key'] }}" /home/web/repo/backup/{{ site }} scp://{{ rsync['user'] }}@{{ rsync['server'] }}/{{ site_name }}/backup
+echo "{{ site_name }}.backup.db.full:1|c" | nc -w 1 -u {{ django['monitor'] }} 2003
+PASSPHRASE="{{ rsync['pass'] }}" duplicity verify scp://{{ rsync['user'] }}@{{ rsync['server'] }}/{{ site_name }}/backup /home/web/repo/backup/{{ site }}
+echo "{{ site_name }}.backup.db.verify:1|c" | nc -w 1 -u {{ django['monitor'] }} 2003
 
-#   duplicity full --encrypt-key="{{ rsync['key'] }}" /home/web/repo/files/{{ site }} scp://{{ rsync['user'] }}@{{ rsync['server'] }}/{{ site_name }}/files
-#   # Send a metric to statsd from bash
-#   # nstielau / send_metric_to_statsd.sh
-#   # https://gist.github.com/nstielau/966835
-#   #
-#   # Useful for:
-#   #   deploy scripts (http://codeascraft.etsy.com/2010/12/08/track-every-release/)
-#   #   init scripts
-#   #   sending metrics via crontab one-liners
-#   #   sprinkling in existing bash scripts.
-#   #
-#   # netcat options:
-#   #   -w timeout If a connection and stdin are idle for more than timeout seconds, then the connection is silently closed.
-#   #   -u         Use UDP instead of the default option of TCP.
-#   echo "{{ site_name }}.backup.full:1|c" | nc -w 1 -u {{ django['monitor'] }} 2003
-#   PASSPHRASE="{{ rsync['pass'] }}" duplicity verify scp://{{ rsync['user'] }}@{{ rsync['server'] }}/{{ site_name }}/files /home/web/repo/files/{{ site }}
-#   echo "{{ site_name }}.backup.verify:1|c" | nc -w 1 -u {{ django['monitor'] }} 2003
+# backup files
+duplicity full --encrypt-key="{{ rsync['key'] }}" /home/web/repo/files/{{ site }} scp://{{ rsync['user'] }}@{{ rsync['server'] }}/{{ site_name }}/files
+echo "{{ site_name }}.backup.files.full:1|c" | nc -w 1 -u {{ django['monitor'] }} 2003
+PASSPHRASE="{{ rsync['pass'] }}" duplicity verify scp://{{ rsync['user'] }}@{{ rsync['server'] }}/{{ site_name }}/files /home/web/repo/files/{{ site }}
+echo "{{ site_name }}.backup.files.verify:1|c" | nc -w 1 -u {{ django['monitor'] }} 2003
 
-echo $DUMP_FILE
-mv $DUMP_FILE /home/web/repo/temp/
+# remove database dump
+rm $DUMP_FILE
